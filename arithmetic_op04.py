cd510714f8fb71e73a152598f2dd3317b41d7804
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:32:19 +0500

    kopaytma

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,7 +1,9 @@
 # Create a variable "a"  and assign it the value of the int 5.
-
+a=5
 # Create a variable "b"  and assign it the value of the int 4.
-
+b=4
 # Create a variable called 'answer'.
 
 # Find the multiplication of a and b then assign to 'answer'
+answer=a*b
+print(answer)
