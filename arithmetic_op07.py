d75870b630ee4652c98908d0c94a9ab01e1a736f
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:24:26 +0500

    natija

--- arithmetic_op07.py
+++ arithmetic_op07.py
@@ -1,9 +1,10 @@
 # Create a variable "a"  and assign it the value of the int 12.
-
+a=12
 # Create a variable "b"  and assign it the value of the int 4.
-
+b=4
 # Create a variable "c"  and assign it the value of the int 1.
-
+c=1
 # Create a variable called 'answer'.
 
 # Find the expression value assign to 'answer'.  a - b + c
+answer=a-b+c
