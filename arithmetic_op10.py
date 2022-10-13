4c62514d1b65d395043cf617ba736686eefe7046
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:19:26 +0500

    darja

--- arithmetic_op10.py
+++ arithmetic_op10.py
@@ -1,5 +1,9 @@
 # Create a variable "n"  and assign it the value of the 4.
+from tkinter import N
 
+
+n=4
 # Create a variable called 'answer'.
 
 # Find the expression value assign to 'answer'. 2^n.
+answer=2**n
