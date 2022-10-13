618c2535d5def92218d2079e2693db9f7c87ed67
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:20:37 +0500

    natija

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -1,11 +1,12 @@
 # Create a variable "a"  and assign it the value of the int 8.
-
+a=8
 # Create a variable "b"  and assign it the value of the int 3.
-
+b=3
 # Create a variable "c"  and assign it the value of the int 2.
-
+c=2
 # Create a variable "d"  and assign it the value of the int 4.
-
+d=4
 # Create a variable called 'answer'.
 
 # Find the expression value assign to 'answer'. a+b - cd .
+answer=a+b-c*d
