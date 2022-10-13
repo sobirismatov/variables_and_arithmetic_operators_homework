4e795df3fc10fd556c97ffa590e770d7f06da13b
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:28:07 +0500

    bolinma

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
+a=72
 # Create a variable "b"  and assign it the value of the int 9.
-
+b=9
 # Create a variable called "answer".
 
 # Find the division of a and b then assign to "answer".
+answer=72/9
