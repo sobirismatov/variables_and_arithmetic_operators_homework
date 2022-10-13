dbac5e6116b94e17b4c5050d9455e7c33730335e
sobirismatov <name@uzcoder96@gmail.com>
2022-10-13 11:33:28 +0500

    ayirma

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 6.
-
+a=6
 # Create a variable "b"  and assign it the value of the int 2.
-
+b=2
 # Create a variable called 'answer'.
 
 # find the difference of a and b then assign to "answer"
+answer=a-b
