26bf8f416777c58f42fc8d8c5e1b02d52a35926c
sobirismatov <name@uzcoder96@gmail.com>
2022-10-14 10:46:40 +0500

    natija

---
+++ formula3.py
@@ -0,0 +1,9 @@
+a=3
+b=4
+c=5
+L=4*(a+b+c)
+S=2*(a*b+b*c+c*a)
+V=a*b*c
+print("Tomonlari",a ,b ,c ,"bo'lgan parallapiped 'tomonlari uzunligi",L ,"ga teng")
+print("Tomonlari",a ,b ,c ,"bo'lgan parallapiped yuzi", S,"ga teng")
+print("Tomonlari",a ,b ,c ,"bo'lgan parallapiped hajmi", V,"ga teng")
