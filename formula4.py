45608f8942b121c8d006ac4014c02baa6bc1d4ec
sobirismatov <name@uzcoder96@gmail.com>
2022-10-14 11:29:43 +0500

    solishtirish

---
+++ formula4.py
@@ -0,0 +1,17 @@
+print('   /\   ')
+print(' b/  \c ')
+print(' /____\ ')
+print('   a    ')
+a=4
+b=2
+c=3
+ha=0.5*2**(1/2)
+#ha a asosga tushirilgan balandlik
+p=a/2+b/2+c/2
+#p yarim peremetr
+S1=a*ha/2
+#s1 asosga tushirilgan balandlik malum hollarda
+S2=(p*(p-a)*(p-b)*(p-c))**(1/2)
+#s2 tomonlari berilgan holda
+print(S1)
+print(S2)
